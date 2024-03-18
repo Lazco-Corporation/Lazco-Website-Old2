8708eb730817660f04883a9f231f59601071b68e
OnCloud <anonymousaaaa41414141@gmail.com>
2024-03-18 16:58:41 +0800

    -

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000"
+    }
+  }
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
