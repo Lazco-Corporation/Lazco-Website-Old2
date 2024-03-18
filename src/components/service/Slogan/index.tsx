2c8286b189272972be51d343303feb7314a42198
OnCloud <anonymousaaaa41414141@gmail.com>
2024-03-18 14:46:44 +0800

    clean project

--- src/components/service/Slogan/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/styles/service/Slogan.module.scss";
-
-export default function Slogan() {
-  return (
-    <div className={styles.Slogan}>
-      <p>提供最好的價格與最完美的服務、完整的售後服務。</p>
-    </div>
-  );
-}
